ac87af810d2f7170d09db5802a1696ec591c4e75
Ptenec_andrey_loh <old_town_OOF@gmai.com>
2023-03-07 19:58:32 +0700

    plhfdcndeqntz hsigsuy

---
+++ Main.py
@@ -0,0 +1,72 @@
+# Игра Shmup - 1 часть
+# Cпрайт игрока и управление
+import pygame
+import random
+
+WIDTH = 480
+HEIGHT = 600
+FPS = 60
+
+# Задаем цвета
+WHITE = (255, 255, 255)
+BLACK = (0, 0, 0)
+RED = (255, 0, 0)
+GREEN = (0, 255, 0)
+BLUE = (0, 0, 255)
+YELLOW = (255, 255, 0)
+
+# Создаем игру и окно
+pygame.init()
+pygame.mixer.init()
+screen = pygame.display.set_mode((WIDTH, HEIGHT))
+pygame.display.set_caption("Shmup!")
+clock = pygame.time.Clock()
+
+class Player(pygame.sprite.Sprite):
+    def __init__(self):
+        pygame.sprite.Sprite.__init__(self)
+        self.image = pygame.Surface((50, 40))
+        self.image.fill(GREEN)
+        self.rect = self.image.get_rect()
+        self.rect.centerx = WIDTH / 2
+        self.rect.bottom = HEIGHT - 10
+        self.speedx = 0
+
+    def update(self):
+        self.speedx = 0
+        keystate = pygame.key.get_pressed()
+        if keystate[pygame.K_LEFT]:
+            self.speedx = -8
+        if keystate[pygame.K_RIGHT]:
+            self.speedx = 8
+        self.rect.x += self.speedx
+        if self.rect.right > WIDTH:
+            self.rect.right = WIDTH
+        if self.rect.left < 0:
+            self.rect.left = 0
+
+all_sprites = pygame.sprite.Group()
+player = Player()
+all_sprites.add(player)
+
+# Цикл игры
+running = True
+while running:
+    # Держим цикл на правильной скорости
+    clock.tick(FPS)
+    # Ввод процесса (события)
+    for event in pygame.event.get():
+        # проверка для закрытия окна
+        if event.type == pygame.QUIT:
+            running = False
+
+    # Обновление
+    all_sprites.update()
+    
+    # Рендеринг
+    screen.fill(BLACK)
+    all_sprites.draw(screen)
+    # После отрисовки всего, переворачиваем экран
+    pygame.display.flip()
+
+pygame.quit()
