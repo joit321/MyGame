6a8a96f7e7d975662f772080364774eb5f738d36
User <Ptenec@foo.com>
2023-03-14 20:00:58 +0700

    добавил управение вверх-вниз и границы чтоб куб не убегал за пределы

--- Main.py
+++ Main.py
@@ -30,20 +30,28 @@ class Player(pygame.sprite.Sprite):
         self.rect = self.image.get_rect()
         self.rect.centerx = WIDTH / 2
         self.rect.bottom = HEIGHT - 10
-        self.speedx = 0
 
     def update(self):
-        self.speedx = 0
         keystate = pygame.key.get_pressed()
         if keystate[pygame.K_LEFT]:
-            self.speedx = -8
+            self.rect.x += -8
         if keystate[pygame.K_RIGHT]:
-            self.speedx = 8
-        self.rect.x += self.speedx
+            self.rect.x += 8
+        if keystate[pygame.K_UP]:
+            self.rect.y += -8
+        if keystate[pygame.K_DOWN]:
+            self.rect.y += 8
+        self.rect.y += 0
+        self.rect.x += 0
         if self.rect.right > WIDTH:
             self.rect.right = WIDTH
         if self.rect.left < 0:
             self.rect.left = 0
+        if self.rect.bottom > HEIGHT:
+            self.rect.bottom = HEIGHT
+        if self.rect.top < 0:
+            self.rect.top = 0
+
 
 all_sprites = pygame.sprite.Group()
 player = Player()
