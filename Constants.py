5a2ddc233d88742e8fc3e975e392501daef7dd9f
andreygurov40 <127208595+andreygurov40@users.noreply.github.com>
2023-04-04 10:31:07 +0700

    переделал управление и добавил таймер

--- Constants.py
+++ Constants.py
@@ -4,8 +4,6 @@ class Constants():
     WIDTH = 480
     HEIGHT = 600
     FPS = 60 #GAME_PATH 
-    score = 0
-    timeofgame = 0
     GAME_PATH = os.path.dirname(__file__)
     RECOURCES_PATH = os.path.join(GAME_PATH, 'Texture')
     all_sprites = pygame.sprite.Group()
